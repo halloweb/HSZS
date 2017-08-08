373b45559ffc358fc9f61f3c65c3fd128d3f1496
halloweb <jiangup@163.com>
2017-08-08 13:57:18 +0800

    企业动态园区预警

--- HSZS/src/components/supervision/goodNews.vue
+++ HSZS/src/components/supervision/goodNews.vue
@@ -1,10 +1,70 @@
 <template>
 	<div class="content-block">
-		企业捷报
+	    <div class="title policy-title">
+           <img src="../../assets/images/good.png" alt="">企业捷报
+        </div>
+		<ul class="list-box">
+                             <li v-for="(item,index) in article">
+                                <div >
+                                  <a class="article-title" href="javascript:void(0);">
+                                    <span class="blue">【{{item.location}}】</span>{{item.title}}
+                                  </a>
+                                  <span class="article-time">{{item.time}}</span>
+                                </div>
+                                <p class="article-content">
+                                  {{item.content}}
+                                </p>
+                             
+                             </li>
+  
+        </ul>
+         <div class="text-center">
+        <el-pagination
+		   @current-change="change"
+		  layout="prev, pager, next"
+		  :total="50">
+		</el-pagination>
+		</div>
 	</div>
 </template>
+<style scoped>
+ .goodNews{
+ 	background-color:#ffffff;
+ }
+	.list-box{
+        margin:0 -24px;
+	}
+	.el-pagination{
+		display:inline-block;
+		
+		margin-top:60px
+	}
+</style>
+
 <script> 
 	export default{
-		
+		data(){
+			return{
+				article:[
+             {
+               location:"信息科技-中科点击",
+               title:'大数据与分析创新峰会',
+               content:"本次会议将涵盖大数据相关的最新行业内应用实践和前沿研究成果，我们将关注于以下主题： 20+ 主题演讲，Workshop和Panss",
+               time:'2017-07-07 00-00-00'
+             },
+             {
+               location:"信息科技-中科点击",
+               title:'大数据与分析创新峰会',
+               content:"本次会议将涵盖大数据相关的最新行业内应用实践和前沿研究成果，我们将关注于以下主题： 20+ 主题演讲，Workshop和Panss",
+               time:'2017-07-07 00-00-00'
+             }
+          ],
+			}
+		},
+		methods:{
+			change(val){
+
+			},
+		},
 	}
 </script>
--- HSZS/src/components/supervision/infoChange.vue
+++ HSZS/src/components/supervision/infoChange.vue
@@ -1,10 +1,74 @@
 <template>
 	<div class="content-block">
-		信息变更预警
+		<div class="title policy-title">
+           <img src="../../assets/images/yanz.png" alt="">信息变更预警
+        </div>
+        <div class="change-search">
+         <el-input placeholder="变更搜索" v-model="searchVal">
+		    <el-button slot="append" icon="search" @click="search"></el-button>
+		  </el-input>
+		 </div>
+	  <div class="table-box"> 
+        <table class="table-zs">
+        	<thead>
+        		<tr>
+        			<th>所属单位</th>
+        			<th>类型标签</th>
+        			<th>更新属性</th>
+        			<th>变更时间</th>
+        			<th>操作</th>
+        		</tr>
+        	</thead>
+        	<tbody>
+        		<tr>
+        			<td>爱心人寿保险股份有限公司</td>
+        			<td>企业</td>
+        			<td>董事</td>
+        			<td>2017-01-02</td>
+        			<td><router-link class="blue" to="/supervision/infoWarning/warningDetails">查看详情</router-link></td>
+        		</tr>
+        		<tr>
+        			<td>爱心人寿保险股份有限公司</td>
+        			<td>企业</td>
+        			<td>董事</td>
+        			<td>2017-01-02</td>
+        			<td><router-link class="blue" to="/supervision/infoWarning/warningDetails">查看详情</router-link></td>
+        		</tr>
+        	</tbody>
+        </table>
+        <p class="loadMore text-center"><a href="javascript:void(0);" class="blue" @click="loadMore">加载更多</a></p>	
+      </div>  
+
 	</div>
 </template>
+<style lang="less" scoped>
+	.change-search{
+         padding:20px 35px;
+         .el-input{
+         	width:300px;
+         }
+	}
+    .table-box{
+    	margin:0 -24px;
+    }
+    .load-more{
+    	margin-top:60px;
+    }
+</style>
 <script> 
 	export default{
-		
+		data(){
+			return{
+				searchVal:''
+			}
+		},
+		methods:{
+			search(){
+
+			},
+			loadMore(){
+
+			}
+		}
 	}
 </script>
